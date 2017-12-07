44faa17c50dc88859c2d762fa80ad156c9975f32
Phoebe Lau <phoebelau7@gmail.com>
2017-12-07 19:12:43 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,6 @@
 class Dog
 @@all = []
-
+attr_accessor :name
 def initialize(name)
   @name = name
   @@all << name
@@ -14,5 +14,26 @@ end
 def self.clear_all
   self.all.clear
 end
+
+end
+
+
+class Dog
+  @@all = []
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+  def self.all
+    @@all.each {|dog| puts dog.name
+    }
+  end
 
 end
